20e409f7c9aa53eee60713a67408434f0a2aef9a
Eding Muhamad <masaruedo@yahoo.com>
2021-11-01 22:23:24 +0700

    emitting events

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,26 @@
 export default {
   data: () => ({
     jsFrameworks: ["react.js", "vue.js", "angular"],
+    backendFrameworks: [],
   }),
-  mounted() {
-    setTimeout(() => {
-      this.jsFrameworks.push("svelte.js");
-    }, 3000);
+  methods: {
+    handleChange(tags) {
+      this.jsFrameworks = [...tags];
+    },
   },
 };
 </script>
 
 <template>
-  <h1>Hello world</h1>
-  <tags-input :selected-tags="jsFrameworks" />
+  <h1>Your favorite frameworks</h1>
+  <div>{{ jsFrameworks }}</div>
+  <tags-input :selected-tags="jsFrameworks" @change="handleChange" />
+
+  <div>{{ backendFrameworks }}</div>
+  <tags-input
+    :selected-tags="backendFrameworks"
+    @change="backendFrameworks = [...$event]"
+  />
 </template>
 
 <style>
